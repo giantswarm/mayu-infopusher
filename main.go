6b6cf10a275c94815667c0aaffa8c53259538f0b
Vaclav Rozsypalek <vaclav@giantswarm.io>
2017-05-15 17:32:48 +0200

    fixing help

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ import (
 
 var url = flag.String("post-url", "", "url to post the host data")
 var help = flag.Bool("help", false, "show help for this tool")
+
 func main() {
 	log.SetFlags(0)
 	flag.Set("logtostderr", "true")
@@ -31,12 +32,12 @@ func main() {
 		println("infopusher version 1.0")
 		return
 	}
-        if *help {
+	if *help {
 		flag.Usage()
-                return
+		return
 	}
 	if *url == "" {
-                fmt.Println("you need to set the parameter post-url")
+		fmt.Println("you need to set the parameter post-url")
 		os.Exit(1)
 	}
 
